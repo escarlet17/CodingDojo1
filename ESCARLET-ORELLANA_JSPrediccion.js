9ef4856fad312240ac0270a73c0d4a6cc1e3cf72
escarlet17 <escarlet.orellana.riquelme@alumnos.sip.cl>
2022-04-07 11:47:52 -0400

    up2

---
+++ ESCARLET-ORELLANA_JSPrediccion.js
@@ -0,0 +1,22 @@
+function myBirthYearFunc(){
+    console.log("Nací en " + 1980);
+
+}
+//la respuesta a la pregunta es que la funcion muestra que "naci en 1980"//
+
+function myBirthYearFunc(EntradaAñoNacimiento){
+    console.log("Nací en " + EntradaAñoNacimiento);
+}
+
+//la respuesta a la variable es el año en que naci en undefined //
+myBirthYearFunc(1998)
+ Nací en 1998
+
+function add(num1, num2){
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+//la respuesta es la suma de num1 y num2  por que num1 = 10 y num2=20 por lo que la variable es igual a 30//
